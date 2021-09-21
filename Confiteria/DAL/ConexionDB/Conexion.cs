a19b3ccfd4de7839c95962ffa3d5eb8ddd6d888a
MaximilianoFarias1987 <maximilianocba07@gmail.com>
2021-09-21 17:55:43 -0300

    Creo Proyecto

---
+++ Confiteria/DAL/ConexionDB/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.ConexionDB
+{
+    public class Conexion
+    {
+        public string ObtenerConexion()
+        {
+            string con = ConfigurationManager.ConnectionStrings["connDb"].ConnectionString;
+            if (string.ReferenceEquals(con, string.Empty))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return con;
+            }
+        }
+    }
+}
